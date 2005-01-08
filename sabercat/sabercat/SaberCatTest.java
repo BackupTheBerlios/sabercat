7ebafe51593be6c3d97153f50dfeb1fc48f60f69
sorenmat <sorenmat>
2005-01-08 13:24:57 +0000

    *** empty log message ***

---
+++ sabercat/sabercat/SaberCatTest.java
@@ -0,0 +1,27 @@
+/*
+ * SaberCatTest.java
+ *
+ * Created on January 8, 2005, 2:16 PM
+ */
+
+package sabercat;
+
+/**
+ *
+ * @author  sorenm
+ */
+public class SaberCatTest {
+    
+    /** Creates a new instance of SaberCatTest */
+    public SaberCatTest() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Sabercat sc = new Sabercat("/home/sorenm/mp3/Mandy Moore - Candy.mp3");
+    }
+    
+}
